6988fe220822096f9f7449bb683341e50ac14928
sKopheK <libor@lipe.cz>
2022-12-03 22:15:09 +0100

    day 3 - part 1

---
+++ 3/app.js
@@ -0,0 +1,24 @@
+// https://adventofcode.com/2022/day/2
+
+const fs = require('fs');
+const { intersection } = require('lodash');
+
+const baseUpper = 'A'.charCodeAt();
+const baseLower = 'a'.charCodeAt();
+
+fs.readFile('input.txt', 'utf8', function(err,data) {
+    const lines = data.split('\r\n');
+    const totalScore = lines.map(function(line) {
+        const center = line.length / 2;
+        const compartment1 = line.substring(0, center).split('');
+        const compartment2 = line.substring(center).split('');
+        const common = intersection(compartment1, compartment2)[0].charCodeAt();
+
+        const priority = common > baseLower
+                ? (common - baseLower + 1)
+                : (common - baseUpper + 27);
+
+        return priority;
+    }).reduce((carry, curr) => carry + curr, 0);
+    console.log(totalScore);
+});
