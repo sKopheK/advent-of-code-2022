5183398036a7cc94979f721a81c6ef272cc79589
sKopheK <libor@lipe.cz>
2022-12-04 13:31:49 +0100

    day 4 - part 1

---
+++ 4/app.js
@@ -0,0 +1,17 @@
+// https://adventofcode.com/2022/day/4
+
+const fs = require('fs');
+const { intersection, range } = require('lodash');
+
+fs.readFile('input.txt', 'utf8', function(_,data) {
+    const lines = data.split('\r\n');
+    const fullyOverlap = lines.map(function(line) {
+        const sections = [...line.matchAll(/(\d+)\-(\d+)/g)]
+            .map(section => range(Number(section[1]), Number(section[2]) + 1));
+        const overlap = intersection(...sections);
+        return overlap.length === sections[0].length
+            || overlap.length === sections[1].length
+                ? 1 : 0;
+    }).reduce((carry, curr) => carry + curr, 0);
+    console.log(fullyOverlap);
+});
