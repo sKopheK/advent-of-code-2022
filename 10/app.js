acb75bb69338b406f81dfac3b6391c0dbd6c4bad
sKopheK <libor@lipe.cz>
2022-12-10 22:46:42 +0100

    day 10 - part 2

--- 10/app.js
+++ 10/app.js
@@ -1,9 +1,11 @@
 // https://adventofcode.com/2022/day/10
 
-const { log } = require('console');
+const { log, table } = require('console');
 const fs = require('fs');
+const { chunk } = require('lodash');
 
 const ADD_CMD = 'addx';
+const CRT_LINE_LEN = 40;
 const interestingCycles = [20,60,100,140,180,220];
 
 fs.readFile(__dirname + '\\input.txt', 'utf8', function(_,data) {
@@ -13,18 +15,19 @@ fs.readFile(__dirname + '\\input.txt', 'utf8', function(_,data) {
     let cycleNumber = 1;
     let signalStrengthSum = 0;
 
+    const screen = Array(CRT_LINE_LEN * 6);
+
     lines.forEach(function(line) {
         const [ cmd, value ] = line.split(' ');
-
-        let cyclesToComplete = 1;
-        if (cmd === ADD_CMD) {
-            cyclesToComplete = 2;
-        }
+        const cyclesToComplete = cmd === ADD_CMD ? 2 : 1;
 
         for (let i = 0; i < cyclesToComplete; i++) {
             if (interestingCycles.indexOf(cycleNumber) >= 0) {
                 signalStrengthSum += cycleNumber * register;
             }
+
+            const pixelId = cycleNumber - 1;
+            screen[pixelId] = Math.abs((pixelId % CRT_LINE_LEN) - register) <= 1 ? '#' : '.';
             cycleNumber += 1;
         }
 
@@ -34,4 +37,5 @@ fs.readFile(__dirname + '\\input.txt', 'utf8', function(_,data) {
     });
 
     log(signalStrengthSum);
+    table(chunk(screen, CRT_LINE_LEN).map(line => line.join('')));
 });
