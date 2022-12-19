950f0908649cb0c259c52d5156d03bc2c6c12f64
sKopheK <libor@lipe.cz>
2022-12-19 23:17:35 +0100

    day 13 - part 1

---
+++ 13/app.js
@@ -0,0 +1,43 @@
+// https://adventofcode.com/2022/day/13
+
+const { log } = require('console');
+const fs = require('fs');
+const { sum } = require('lodash');
+
+const compare = (left, right) => {
+    if (!Array.isArray(left) && !Array.isArray(right)) {
+        return left - right;
+    }
+
+    if (Array.isArray(left) && Array.isArray(right)) {
+        let i = 0;
+        for (; i < left.length; i++) {
+            if (i >= right.length) {
+                return false;
+            }
+            const result = compare(left[i], right[i]);
+            if (result === 0) {
+                continue;
+            }
+            return result;
+        }
+        return left.length - right.length;
+    }
+
+    if (Array.isArray(left) && !Array.isArray(right)) {
+        return compare(left, [right]);
+    }
+
+    if (!Array.isArray(left) && Array.isArray(right)) {
+        return compare([left], right);
+    }
+};
+
+fs.readFile(__dirname + '\\input.txt', 'utf8', function(_,data) {
+    const pairs = data.split('\r\n\r\n');
+    const correctOrder = pairs.map(function(pair, index) {
+        const [left, right] = pair.split('\r\n');
+        return compare(eval(left), eval(right)) < 0 ? index + 1 : 0;
+    });
+    log(sum(correctOrder));
+});
